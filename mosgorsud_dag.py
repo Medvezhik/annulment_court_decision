61fe4483ced79799c30efe4f66f7bcfe9500be67
podlaskin <114940691+podlaskin@users.noreply.github.com>
2023-01-06 12:36:22 +0300

    dag update

--- mosgorsud_dag.py
+++ mosgorsud_dag.py
@@ -11,8 +11,6 @@ import docx
 import os
 import re
 import pandas as pd
-from joblib import load
-import ru_core_news_sm as ru
 from sklearn.metrics import roc_auc_score
 
 dir_doc = "/home/user/sud/doc/"
@@ -24,7 +22,7 @@ def get_from_url(fn, url):
     if(os.path.exists(fn)):
         print(fn)
         return open(fn, 'rb').read()
-    
+
     time.sleep(random.randint(1, 3))
     ua = UserAgent()
     header = {'User-Agent':str(ua.random)}
@@ -78,7 +76,7 @@ def cut_word(text):
 
 def get_page(page, date1, date2):
     n = 0
-    
+
     content = get_from_url('', f'https://mos-gorsud.ru/mgs/search?caseDateFrom=&caseDateTo=&caseFinalDateFrom={date1}&caseFinalDateTo={date2}&caseLegalForceDateFrom=&caseLegalForceDateTo=&caseNumber=&letterNumber=&category=&codex=&courtAlias=mgs&docsDateFrom=&docsDateTo=&documentStatus=&documentText=&documentType=&instance=2&judge=&participant=&processType=2&publishingState=&uid=&year=&formType=fullForm&page={page}')
 
     try:
@@ -96,7 +94,7 @@ def get_page(page, date1, date2):
             url = parse_info(info)
             if url != '':
                 get_from_url(dir_doc + name + '.doc', url)
-                
+
     return False
 
 def get_info(name):
@@ -129,11 +127,11 @@ def download():
         os.mkdir(dir_case)
     except FileExistsError:
         print("FileExistsError")
-        
+
     page = 1
     date1 = datetime.date.today()-datetime.timedelta(days=13)
     date2 = datetime.date.today()-datetime.timedelta(days=7)
-    
+
     done = False
     while not done:
         try:
@@ -162,10 +160,10 @@ def make_csv():
                     print('not open', name)
                 else:
                     print('docx', name)
-                
+
             if text=='':
                 continue
-        
+
             d = get_info(dir_info + name[0:8] + 'htm')
             file.write(getd(d, 'Уникальныйидентификатордела') + ',')
             file.write(getd(d, 'Стороны') + ',')
@@ -178,19 +176,15 @@ def make_csv():
             d = get_info(dir_case + name[0:8] + 'htm')
             file.write(getd(d, 'Результатрассмотрения') + ',')
             file.write(getd(d, 'Основаниерешениясуда') + ',')                
-    
+
             text = text.replace('"', ' ').replace('«', ' ').replace('»', ' ').replace('\n', ' ')
             file.write('"' + text + '"\n')
-        
+
     file.close()
 
     shutil.rmtree(dir_case)
     shutil.rmtree(dir_info)
     shutil.rmtree(dir_doc)
-
-lem = ru.load()
-lr = load('/home/user/sud/logreg.joblib')
-tfidf = load('/home/user/sud/tfidf.joblib')    
 
 def verdict_to_int(verdict_str):
     if 'Оставить' in verdict_str.split():
@@ -209,11 +203,21 @@ def convert(text):
     text = text.lower()
     text = " ".join(re.sub(r'[^u"а-яА-Я"]', ' ', text).split()) #удаляем не буквы
     text = " ".join(re.sub(r'(^|\s)\w{1}(\s\w{1})*($|\s)', ' ', text).split()) #удаляем слова из 1 буквы
-    
+
     doc = lem(text)
     text = " ".join(token.lemma_ for token in doc)
     return text
-    
+
+def get_predict(text: str):
+    request_url = "http://158.160.12.150/predict"
+    response = requests.get(request_url, json={"text": text})
+    if response.status_code != 200:
+        print('server error', response.status_code)
+        return float(0)
+
+    print(response.json()["predict"])
+    return float(response.json()["predict"])
+
 def monitoring_metrics():
     data = pd.read_csv(csv_name)
     data['target'] = data['verdict_up'].apply(verdict_to_int)
@@ -221,18 +225,16 @@ def monitoring_metrics():
     data['pdf'] = data['text'].apply(find_pdf)
     data = data[data['pdf']==0]
     data.drop_duplicates(inplace=True)
-    data['clear_text'] = data['text'].apply(convert)
+    data['preds'] = data['text'].apply(get_predict)
 
-    features = tfidf.transform(data['clear_text'])
-    preds = lr.predict_proba(features)[:, 0]
     target = data['target'].values
+    preds = data['preds'].values
     rocauc = roc_auc_score(target, preds)
     print(rocauc)
-        
-with DAG(dag_id='mosgorsud_weekly_dag', start_date=datetime.datetime(2022, 10, 20, 6, 0), end_date=datetime.datetime(2023, 10, 26, 6, 0), schedule="0 6 * * 3") as dag:
+
+with DAG(dag_id='mosgorsud_weekly_dag', start_date=datetime.datetime(2022, 10, 20, 6, 0), end_date=datetime.datetime(2023, 10, 26, 6, 0), schedule="0 6 * * 6") as dag:
     download_operator = PythonOperator(task_id='download', python_callable=download)
     make_csv_operator = PythonOperator(task_id='make_csv', python_callable=make_csv)
     monitoring_operator = PythonOperator(task_id='monitoring', python_callable=monitoring_metrics)
 
     download_operator >> make_csv_operator >> monitoring_operator
-    
